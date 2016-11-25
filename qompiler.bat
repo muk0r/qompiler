afab0dd748fba8eb5e48cadf1193f7b9122c6e7d
muk0r <marcuster@gmail.com>
2016-11-25 08:23:10 -0600

    Update qompiler.bat

--- qompiler.bat
+++ qompiler.bat
@@ -2,9 +2,9 @@
 
 Title Qompiler v 0.25
 
-If Not Exist "*"\qbsp.exe GoTo NoTools
-If Not Exist "*"\vis.exe GoTo NoTools
-If Not Exist "*"\light.exe GoTo NoTools
+If Not Exist tools\qbsp.exe GoTo NoTools
+If Not Exist tools\vis.exe GoTo NoTools
+If Not Exist tools\light.exe GoTo NoTools
 If Not Exist *.MAP GoTo NeedMaps
 If Not Exist Qompiled\Nul MD Qompiled
 If "%1"=="DoIt" GoTo Qompiler
@@ -26,9 +26,9 @@ GoTo End
 :Qompiler
 Echo Recompiling map %2...
 Echo Recompiling %2>>Qompiler.txt
-bin\qbsp.exe %_qbsp% %2>>Qompiler.txt
-bin\vis.exe %_vis% %2>>Qompiler.txt
-bin\light.exe %_light% %2>>Qompiler.txt
+tools\qbsp.exe %_qbsp% %2>>Qompiler.txt
+tools\vis.exe %_vis% %2>>Qompiler.txt
+tools\light.exe %_light% %2>>Qompiler.txt
 Echo Finished map %2>>Qompiler.txt
 Echo.>>Qompiler.txt
 Echo.>>Qompiler.txt
